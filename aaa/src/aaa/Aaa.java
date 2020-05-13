3b14948645403d6095ea48fcaf6471057d2a3a2c
user <user@DESKTOP-LBCE4QB>
2020-05-14 01:24:05 +0900

    별찍기2

--- aaa/src/aaa/Aaa.java
+++ aaa/src/aaa/Aaa.java
@@ -11,7 +11,13 @@ for(int i=1; i<=5; i++) {
 	}
 	System.out.printf("\n");
 }
-
+	for(int i=6; i>=1; i--) {
+		for(int k=1; k<=i; k++) {
+			System.out.printf("*");
+		}
+		System.out.printf("\n");
 	}
+}
 
+	
 }
