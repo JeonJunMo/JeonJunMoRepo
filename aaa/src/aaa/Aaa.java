5c50e716895af03f19d7060b2b2a50c0c9926b84
user <user@DESKTOP-LBCE4QB>
2020-05-14 01:18:13 +0900

    구구단

--- aaa/src/aaa/Aaa.java
+++ aaa/src/aaa/Aaa.java
@@ -4,7 +4,14 @@ public class Aaa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("aa!");
+for(int i=2; i<=9; i++) {
+	for(int k=1; k<=9; k++) {
+		System.out.printf("%dX%d=%d", i , k, i*k);
+		System.out.printf("\n");
+	}
+	System.out.printf("\n");
+}
+
 	}
 
 }
