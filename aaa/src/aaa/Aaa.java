92583795128dc8e88b1f09dac1d05677ea033237
user <user@DESKTOP-LBCE4QB>
2020-05-14 20:52:12 +0900

    별찍기3

--- aaa/src/aaa/Aaa.java
+++ aaa/src/aaa/Aaa.java
@@ -3,21 +3,23 @@ package aaa;
 public class Aaa {
 
 	public static void main(String[] args) {
+		int k;
 		// TODO Auto-generated method stub
-for(int i=1; i<=5; i++) {
-	for(int k=1; k<=i; k++) {
-		System.out.printf("*");
-		
-	}
-	System.out.printf("\n");
-}
-	for(int i=6; i>=1; i--) {
-		for(int k=1; k<=i; k++) {
-			System.out.printf("*");
+for(int i=6; i>0; i--){
+	for(k=0; k<i; k++){
+		System.out.printf(" ");
 		}
-		System.out.printf("\n");
+		for(k=6; k>=i; k--){
+		System.out.printf("*");
+		}
+	System.out.printf("\n");
+	}
 	}
 }
 
-	
-}
+
+
+
+
+
+
